051e17acf6feb747969c1b263c1ce96b81d04c21
AGQA2024 <codesoftware2579@gmail.com>
2024-04-16 11:23:21 +0100

    File push

--- dock/main.tf
+++ dock/main.tf
@@ -12,3 +12,13 @@ provider "docker" {}
 resource "docker_image" "web" {
   name = "httpd:latest"
 }
+
+resource "docker_container" "web" {
+  name = "webserver"
+  image = docker_container.web.image_id
+
+  ports {
+    internal = 80 
+    external = 8080
+  }
+}
